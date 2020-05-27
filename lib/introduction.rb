dacc87b8c0a261509384d6f51397228511dba95c
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-05-27 18:32:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,5 @@
-def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and i am learning to program in #{language}"
+def introduction(name)
+  puts "Hi, my name is #{name} 
 end
+
+introduction (josh)
